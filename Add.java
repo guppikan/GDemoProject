8b948ae37fc96818e2c13e80983cb5eb4aec4ac3
guppi4u <122578051+guppi4u@users.noreply.github.com>
2023-01-14 11:26:28 +0000

    First commit on feature1 branch

---
+++ Add.java
@@ -0,0 +1,11 @@
+class Add{
+    public static void main(String[] args) {
+        int x=10;
+        int y=20;
+
+        int sum;
+        sum=x+y;
+
+        System.out.println("Sum of two numbers :"+sum);
+    }
+}
