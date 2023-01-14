34822563c14f8bd31d9f133e8fcca2d74bc9b749
guppi4u <122578051+guppi4u@users.noreply.github.com>
2023-01-14 11:53:19 +0000

    Adding diffrence java program

---
+++ Diff.java
@@ -0,0 +1,11 @@
+class Diff{
+    public static void main(String[] args) {
+        int x=100;
+        int y=20;
+
+        int diff;
+        diff=x-y;
+
+        System.out.println("Diffrence of two numbers :"+diff);
+    }
+}
